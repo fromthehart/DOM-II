6f5a4d525518449389c6312cc14722ab478e892c
fromthehart <fromthehart@pm.me>
2019-05-14 15:53:47 -0400

    Change H1 color when wheeled over

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+// Return a random number from 0 to max
+const randomIndex = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
+// Change H1 color on mouseover / mouseleave
 const funHeaders = document.querySelectorAll('h2');
 funHeaders.forEach(h2 => {
   h2.addEventListener('mouseover', e => {
@@ -7,3 +13,9 @@ funHeaders.forEach(h2 => {
     h2.classList.toggle('orange');
   });
 });
+
+// Change H1 color to a random color when wheeled over
+const funnestHeader = document.querySelector('h1');
+funnestHeader.addEventListener('wheel', e=> {
+  funnestHeader.style.color = ['red', 'green', 'purple', 'orange'][randomIndex(4)];
+})
