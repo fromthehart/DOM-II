662db68d94dcc06a5b76b8e10cdce8dafb06d42a
fromthehart <fromthehart@pm.me>
2019-05-14 15:55:15 -0400

    After 10 seconds, the fun bus img will be swapped with another

--- js/index.js
+++ js/index.js
@@ -19,3 +19,12 @@ const funnestHeader = document.querySelector('h1');
 funnestHeader.addEventListener('wheel', e=> {
   funnestHeader.style.color = ['red', 'green', 'purple', 'orange'][randomIndex(4)];
 })
+
+// Our bus is ready to go. If you don't board now, it'll leave without you!
+const impatientBus = document.querySelector('header img');
+impatientBus.addEventListener('load', e => {
+  window.setTimeout(() => {
+    console.log('got here');
+    impatientBus.src = '../img/impatient-bus.jpg';
+  }, 10000);
+})
