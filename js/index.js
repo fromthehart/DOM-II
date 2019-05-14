e3702b470f92d9ddd6bdbf8712c4cea179b91bf0
fromthehart <fromthehart@pm.me>
2019-05-14 15:31:26 -0400

    Change color of H2 elements on mouseover / mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+const funHeaders = document.querySelectorAll('h2');
+funHeaders.forEach(h2 => {
+  h2.addEventListener('mouseover', e => {
+    h2.classList.toggle('orange');
+  });
+  h2.addEventListener('mouseleave', e => {
+    h2.classList.toggle('orange');
+  });
+});
